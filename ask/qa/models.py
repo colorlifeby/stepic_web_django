26c1b7bd32b391a2595c25699517a71bf6383326
colorlifeby <a.o.gusakov@gmail.com>
2016-11-20 20:40:28 +0300

    Добавлены модели и менеджер, база прописана.

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,23 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuestionManager(models.Manager):
+        def new():
+            xyz=1
+        def popular():
+            xyz=2
+
+class Question(models.Model):
+	objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(User, related_name="question_like", blank=True)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
