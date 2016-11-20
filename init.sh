f3bfda9fba83e441ff9384f8c2081fa13e440c04
colorlifeby <a.o.gusakov@gmail.com>
2016-11-20 20:48:59 +0300

    init.sh добавлены работа с базой

--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
+# nginx
 sudo ln -sf /home/box/web/etc/box.conf  /etc/nginx/conf.d
-#sudo nginx -s reload
 sudo /etc/init.d/nginx restart
+
+# gunicorn
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/wsgi.py /etc/gunicorn.d/wsgi.py 
 sudo /etc/init.d/gunicorn restart
+
+# mysql
+sudo /etc/init.d/mysql restart
+sudo mysql -uroot -e "DROP DATABASE IF EXISTS ask"
+sudo mysql -uroot -e "CREATE DATABASE ask"
+sudo mysql -uroot -e "CREATE USER 'askuser'@'localhost' IDENTIFIED BY '11111'"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'askuser'@'localhost' IDENTIFIED BY '11111'" 
