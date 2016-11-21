f77213375585d277ae8f482ea95f3da5c4915e6d
Your Name <a.o.gusakov@gmail.com>
2016-11-21 15:17:54 +0300

    Add question_list in view

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404,HttpResponse, HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage
+from django.core.urlresolvers import reverse
+from qa.models import Question
 
-# Create your views here.
-def test(request, *args, **kwargs):
-	return HttpResponse('OK')
+#--------------------------------------------------------------------
+def test(request, *args, **kwargs): from django.contrib.auth 
+    return HttpResponse('UNDER CONSTRUCTION! COMING SOON!')
+
+
+#--------------------------------------------------------------------
+def paginate(request, qs):
+    try:
+         limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+    paginator = Paginator(qs, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page, paginator
+ 
+
+#---------------------------------------------------------------
+def question_list(request):
+    qs = Question.objects.all()
+    qs = qs.order_by('-added_at')
+    page, paginator = paginate(request, qs)
+    paginator.baseurl = reverse('question_list') + '?page='
+    return render(request, 'list.html', {
+        'questions': page.object_list,
+        'page': page,
+        'paginator': paginator,
+    }) 
